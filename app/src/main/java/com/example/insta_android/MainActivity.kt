dc78d4ae4814d98c71a81e825b158ab89ca86660
Pavel Zaitsev <pavel@arslogi.ca>
2019-08-28 16:51:02 +0300

    progress, need to use exif reader

--- app/src/main/java/com/example/insta_android/MainActivity.kt
+++ app/src/main/java/com/example/insta_android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.insta_android
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
@@ -14,9 +15,18 @@ import android.support.v4.content.FileProvider
 
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+
+import android.graphics.BitmapFactory
+import android.widget.ImageView
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.media.Image
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +38,16 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             dispatchTakePictureIntent()
         }
+        System.out.printf(">>>> path: %s\n",Environment.getExternalStorageDirectory().getPath().toString())
+        val root = Environment.getExternalStorageDirectory().getPath().toString()
+        val dir = File(root + "/INSTA")
+        dir.mkdirs()
+        val outfile = File(dir, "FIle.txt")
+        try {
+            val f = FileOutputStream(outfile)
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        }
     }
     var currentPhotoPath: String = ""
 
@@ -35,7 +55,9 @@ class MainActivity : AppCompatActivity() {
     private fun createImageFile(): File {
         // Create an image file name
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val root = Environment.getExternalStorageDirectory().getPath().toString()
+        val dir = File(root + "/INSTA")
+        val storageDir: File = dir //getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
             "JPEG_${timeStamp}_", /* prefix */
             ".jpg", /* suffix */
@@ -48,8 +70,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     val REQUEST_TAKE_PHOTO = 1
-
-
+    var photoUri: Uri? = null
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             // Ensure that there's a camera activity to handle the intent
@@ -60,7 +81,7 @@ class MainActivity : AppCompatActivity() {
                 } catch (ex: IOException) {
                     // Error occurred while creating the File
                     System.out.println("Error filing")
-                    null
+                        null
                 }
                 // Continue only if the File was successfully created
                 photoFile?.also {
@@ -69,20 +90,59 @@ class MainActivity : AppCompatActivity() {
                         "com.example.insta_android.fileprovider",
                         it
                     )
+                    photoUri = photoURI
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                     startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                    System.out.println("HEY INTENT")
                 }
             }
         }
     }
 
+    public override fun onActivityResult(reqCode: Int, resCode: Int, data: Intent?){
+        setPic()
+    }
+
+    private fun setPic() {
+        // Get the dimensions of the View
+        val imageView: ImageView =  findViewById(R.id.imageView2)
+        val targetW: Int = imageView.width
+        val targetH: Int = imageView.height
+
+        val bmOptions = BitmapFactory.Options().apply {
+            // Get the dimensions of the bitmap
+            inJustDecodeBounds = true
+
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+
+            // Determine how much to scale down the image
+            val scaleFactor: Int = Math.min(photoW / targetW, photoH / targetH)
+
+            // Decode the image file into a Bitmap sized to fill the View
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inPurgeable = true
+        }
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
+            galleryAddPic()
+            imageView.setImageBitmap(bitmap)
+        }
+    }
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
+        }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
-
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
